2e3d97c678d690c87ef02e23d2eea66e285fc584
Amber Jain <i.amber.jain@gmail.com>
2013-09-28 10:31:45 +0530

    temporarily strip down models.py to a simple table to fix South migration issue

--- carpet/votein_server/models.py
+++ carpet/votein_server/models.py
@@ -2,14 +2,3 @@ from django.db import models
 
 class Vote(models.Model):
     voter_ID = models.CharField(max_length=50)      #can be external like Facebook ID
-    DOWN = 'Dw'
-    UP = 'Up'
-    VOTE_CHOICES = (
-        (DOWN, 'Down'),
-        (UP, 'Up'),
-    )
-    voteType = models.CharField(max_length=2,
-                                      choices=VOTE_CHOICES,
-                                      default=UP)
-    def __unicode__(self):
-        return self.voteType in (self.UP, self.DOWN)
