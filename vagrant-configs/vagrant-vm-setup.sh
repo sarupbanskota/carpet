47f74edc42053f7350f15e2ffa04ef5c17b1eb9d
Amber Jain <i.amber.jain@gmail.com>
2013-09-28 07:10:37 +0530

    Install postgresql in vagrant VM

--- vagrant-configs/vagrant-vm-setup.sh
+++ vagrant-configs/vagrant-vm-setup.sh
@@ -12,4 +12,17 @@ sudo apt-get -y install python-pip
 sudo apt-get -y install git
 sudo apt-get -y install tmux
 
+if [ ! -f /home/vagrant/.vagrant_do_not_delete ]; then
+    # Locale stuff (for PostgreSQL)
+    # Source: http://www.softr.li/blog/2012/05/22/chef-recipe-to-install-a-postgresql-server-on-a-machine-configured-with-en_us-locales
+    export LANGUAGE="en_US.UTF-8"
+    export LANG="en_US.UTF-8"
+    export LC_ALL="en_US.UTF-8"
+    locale-gen en_US.UTF-8
+    sudo dpkg-reconfigure locales
+fi
 
+sudo apt-get -y install postgresql-9.1 
+sudo apt-get -y install postgresql
+sudo apt-get -y install postgresql-client
+sudo apt-get -y install python-psycopg2
