35e827efd65ba9dc2b1e0a1cf3112eca3b3fb38d
Ace <abhas.bodas@gmail.com>
2013-09-28 23:02:46 -0700

    added some imports in views

--- carpet/votein_server/views.py
+++ carpet/votein_server/views.py
@@ -1,7 +1,9 @@
 # Create your views here.
 from votein_server.models import *
 from django.views.generic import DetailView, ListView, TemplateView
+from django.template import Context, RequestContext, loader
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
 
 class ErrorView(TemplateView):
 	def get_context_data(self):
