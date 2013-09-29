c89e94b98b4b68104b6b1ebfc9b4155c80135034
Amber Jain <i.amber.jain@gmail.com>
2013-09-30 04:25:26 +0530

    remove import *

--- carpet/votein_server/views.py
+++ carpet/votein_server/views.py
@@ -1,9 +1,13 @@
 # Create your views here.
-from votein_server.models import *
+from votein_server.models import Article, Opinion, Vote
 from django.views.generic import DetailView, ListView, TemplateView
 from django.template import Context, RequestContext, loader
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 class ErrorView(TemplateView):
     def get_context_data(self):
@@ -22,3 +26,18 @@ def AddVote(self, vote):
     elif vote.voteType == Vote.VOTE_CHOICES.Down:
         self.opinionVotes.downVotes.add(vote)
     self.save()
+
+def home(request):
+    return render(request, "votein_server/index.html")
+
+def about(request):
+    return render(request, "votein_server/about.html")
+
+def contact(request):
+    return render(request, "votein_server/contact.html")
+
+def login(request):
+    return render(request, "votein_server/login.html")
+
+def signup(request):
+    return render(request, "votein_server/signup.html")
